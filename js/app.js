8cc2b5afff22a2f62a7338d7aa2535289df8fbe1
brian <bml@rogers.com>
2013-06-29 12:19:21 -0400

    bogus

---
+++ js/app.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function() {
+
+	$('#copyright-year').text((new Date()).getFullYear());
+
+	//
+	// expand/collapse work category links
+	//
+	$('.category').click(function() {
+		$('.category_container').slideUp();
+		$(this).toggleClass('open_cat');
+		if ($(this).next('.category_container').css('display') === 'none') {
+			$(this).next('.category_container').slideDown();
+		}
+	});
+});
